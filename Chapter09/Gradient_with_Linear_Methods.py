d047a59b9bc08ab67c2b3965a2021b3c46c16d25
Quang Nguyen <quangnguyen136@gmail.com>
2018-06-27 13:51:26 +0900

    Initialize PJ: Stochastic and Semi-Stochastic Gradient with Linear Methods

---
+++ Chapter09/Gradient_with_Linear_Methods.py
@@ -0,0 +1,4 @@
+# Example 6.6 page 108 in Reinforcement Learning: An Introduction Book
+# PhD Student: Nguyen Dang Quang, Computer Science Department, KyungHee University, Korea.
+# Stochastic and Semi-Stochastic Gradient with linear methods.
+
